0bd2f8d1a0c887a8cd18453438b8d114fae55c5b
Harry Wong <RedAndBlueEraser@outlook.com>
2016-01-21 14:54:45 +1100

    Added RotatingY and RotatingX objects.

---
+++ Rotating.js
@@ -0,0 +1,26 @@
+/*
+ * Rotating.js
+ * Version 20160121
+ * Written by Harry Wong (RedAndBlueEraser)
+ */
+
+/**
+ * Operates on HTML elements to smoothly and continuously rotate webpage
+ * elements.
+ */
+
+/**
+ * The collection of constants and functions required to smoothly and
+ * continuously rotate webpage elements on the Y-axis, as an Object object.
+ */
+var RotatingY = {};
+
+//TODO
+
+/**
+ * The collection of constants and functions required to smoothly and
+ * continuously rotate webpage elements on the X-axis, as an Object object.
+ */
+var RotatingX = {};
+
+//TODO
